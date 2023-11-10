3b3e7b315e791855d07bc534e87a1ea7304f4d65
hikaru <h.matsuba.sys23@morijyobi.ac.jp>
2023-11-10 13:05:24 +0900

    Main.javaを２人分計算できるように変更

--- Main.java
+++ Main.java
@@ -16,5 +16,29 @@ public class Main {
 	 }else{
 		 System.out.println("あなたは太っていません！この調子で頑張りましょう(>-<)");
 	 }
+	 System.out.println("もう一人もしらべますか？");
+	 System.out.println("調べるなら1 を");
+	 System.out.println("調べないなら2　を入力してください。");
+	 int mouhitori = Integer.parseInt(scanner.nextLine());
+	 if(mouhitori == 1){
+		  System.out.println("自分のBMIを知ろう！！");
+         System.out.println("身長:");
+         sintyou = Integer.parseInt(scanner.nextLine());
+         System.out.println("体重:");
+         taizyuu = Integer.parseInt(scanner.nextLine());
+         sintyou = sintyou / 100;
+                nizyou = sintyou * sintyou;
+                BBmi = taizyuu / nizyou;
+                System.out.println(String.format("%.2f",BBmi));
+                if(25 <= BBmi){
+                        System.out.println("あなたは太っています。痩せましょう！");
+         }else{
+                 System.out.println("あなたは太っていません！この調子で頑張りましょう(>-<)");
+         }
+
+}else{
+System.out.println("お疲れさまでした");
 }
+ }
 }
+
